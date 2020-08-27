5bcde637ae119090afd74eb5045811a41a8e0f63
Jacob Faircloth <fairclothjr@grizzlies.adams.edu>
2020-08-27 16:47:13 -0600

    Assignment 0 Changes 1.1

--- helloCSCI206.java
+++ helloCSCI206.java
@@ -7,6 +7,8 @@ Assignment: 0
 public class helloCSCI206 {
     public static void main(String[] args) {
 
-        System.out.println("Hello CSCI 206 World");
+        System.out.println("Hello CSCI 206 World how is your day going so far");
+        System.out.println("Have a great rest of your week.");
+        System.out.println("My name is Jacob Faircloth");
     }
 }
